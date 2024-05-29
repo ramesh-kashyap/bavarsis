c28c766d9f40013ca04ad616cf69c9bae3bcd663
rajkumarkashyap3582 <rajkumarkashyap3582@gmail.com>
2024-05-29 17:39:12 +0530

    login update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,112 +1,127 @@
+
 <!DOCTYPE html>
-<html>
-
+<html lang="en">
 <head>
-    <!-- Google Tag Manager -->
+    <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Welcome</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Overpass:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;family=Russo+One&amp;display=swap" rel="stylesheet">
+<link rel="stylesheet" href="{{asset('')}}assets/css/style121312312223.css">
+        <link rel="icon" type="image/x-icon" href="{{asset('')}}assets/icons/fav.png">
+        <link
+                rel="stylesheet"
+                href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css"
+        />
 
 
-    <!-- End Google Tag Manager -->
-    <meta charset="utf-8">
-    <title>{{siteName()}}</title>
-    <base href="{{asset('')}}" />
-    <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-iconf9e3.png?v=1.1">
-    <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32f9e3.png?v=1.1">
-    <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16f9e3.png?v=1.1">
-    <link rel="manifest" href="/favicons/site.webmanifest?v=1.1">
-    <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg?v=1.0" color="#5bbad5">
-    <meta name="msapplication-TileColor" content="#603cba">
-    <meta name="theme-color" content="#ffffff">
+        <style>.success_info{
+    padding: 20px 12px;
+border-radius: 10px;
+width: 98%;
+background: #d7a3e6;
+    color: #fff;
+    font-size: 14px;
+}
 
-    <meta content="width=device-width, initial-scale=1" name="viewport">
-    <meta content="ie=edge" http-equiv="x-ua-compatible">
 
-    <link rel="stylesheet" href="{{asset('')}}assets/css/libs.css">
-    <link rel="stylesheet" href="{{asset('')}}assets/css/app54f9.css?v=1.009995">
-    <link rel="stylesheet" href="{{asset('')}}assets/css/customb361.css?v=1.000099999">
-    <!--[if lt IE 9]>
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script><![endif]-->
+.danger_info{
+    padding: 20px 12px;
+    border-radius: 10px;
+    width: 98%;
+    background: rgb(116, 26, 26);
+    color: #fff;
+    font-size: 14px;
+}
+
+          .clasas_form{
+            width: 100%;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+gap: 20px;
+          }
+        </style>
 
 </head>
-
 <body>
- 
-    <div class="copy-success">
-        <svg>
-            <use xlink:href="/assets/img/sprite.svg#check"></use>
-        </svg>
-    </div>
-    <div class="wrapper">
-        <section class="sign">
-            <div class="container"> <a class="sign-logo" href="/"><img style="    width: 200px;" src="/assets/img/Clogo.png?v=1.0"
-                        alt="logo" /></a>
-                <div class="row">
-                    <div class="col-lg-6 offset-lg-3">
-                        <form action="{{ route('login') }}" method="POST" name="login_frm">
-                            {{ csrf_field() }}
-                            <div class="sign-box">
-                                <div class="sign-img"> <img src="/assets/img/contacts-form-img.png"
-                                        alt="contacts-form-img" /></div>
-                                <div class="sign-title">Sign IN<p>Welcome to {{siteName()}}</p>
-                                </div>
-                                <div class="contacts-form__inputs"><label
-                                        class="contacts-form__input contacts-form__input_icon"><input type="text"
-                                            name="username" value="" placeholder="Your Login Id"><svg>
-                                            <use xlink:href="/assets/img/sprite.svg#user"></use>
-                                        </svg></label>
-                                        
-                                        <label
-                                        class="contacts-form__input contacts-form__input_icon"><input type="password"
-                                            name="password" value="" placeholder="Your Password"><svg>
-                                            <use xlink:href="/assets/img/sprite.svg#pass"></use>
-                                        </svg></label>
-                                        
-                                          <label
-                                        class="contacts-form__input contacts-form__input_icon"><input type="text"
-                                            name="captcha" value="" placeholder="Enter Captcha"><svg>
-                                            <use xlink:href="/assets/img/sprite.svg#pass"></use>
-                                        </svg></label>
-                                        
-                                        <label
-                                        class="contacts-form__input contacts-form__input_icon">
-                                              @php
-                                                $captcha = getCustomCaptcha($height = 46, $width = '100%', $bgcolor = '#003');
-                                                @endphp 
-
-                                              @php echo  $captcha @endphp
-                                            @if(reCaptcha())
-                                            @php echo reCaptcha(); @endphp
-                                            @endif
-                                            
-                                        </label>
-                                        
-                                        
-                                        <input name="__Cert" value="dbfcbcc5" type="hidden"></div><a
-                                    class="sign-link" href="/forgot-password">Forgot your password?</a><button
-                                    name="login_frm_btn" type="submit" class="main-btn main-btn_orange main-btn_m">Sign
-                                    IN</button>
-                                <div class="sign-bot">Don't you have an account yet?<a href="register"> Sign Up</a></div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            <div class="sign-footer">Copyright © 2023 etriton.co All rights reserved</div>
+<div class="wrapper wrapper--start">
+    <main class="start">
+        <section class="start__image-side">
+                        <img src="{{asset('')}}assets/img/reg_img1.png" alt="" style="width: auto;">
+                        
         </section>
-    </div>
-@include('partials.notify')
-    <script src="{{asset('')}}assets/js/libsb883.js?v=1.01"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.maskedinput/1.4.1/jquery.maskedinput.min.js"></script>
-<script src="{{asset('')}}assets/js/app437f.js?v=1.000991"></script>
+        <section class="start__form-side">
+<!--                <div class="language__container"></div>-->
+            <div class="start__form-header">
+               <a href="index.html"> <img src="{{asset('')}}assets/icons/logo-main.svg" alt=""></a>
+            </div><div class="start__form start__form--login"><div class="start__form-title"><div class="start__main-text _one _f48">Welcome
 
-    <script>
-        var images = document.getElementsByTagName('img');
-        for (var i = 0; i < images.length; i++) {
-            var imageUrl = images[i].src;
-            images[i].src = imageUrl + '?v=1.0';
+            </div><div class="start__sub-text _medium _f16"><span>Don&#039;t have an account? 
+
+            </span>&nbsp; <a href="page.php=create.html"  class="start__link">Register
+
+            </a></div></div><div class="start__form-inputs">
+                <form   class="signin_validate clasas_form" method="POST"	
+             action="{{route('login')}}"><style type="text/css">.
+              {{ csrf_field() }}
+             form-control{margin-bottom: 25px;}.bg-danger{color: #fff;border-radius: 50px;}.err{color: rgb(208, 60, 60);}</style>
+             <!-- <fieldset><table class="table" style=""> --><div class="start__main-inputs-wrapper">
+                <div class="settings__password-new-repeat">	<tr><div class="hide_box_0">
+                    <style type="text/css"></style><div class="form-input">
+                        <input name="username" placeholder="Usernmae"  value="" size="20" type="text" 
+                        class="string_small start__input _semi _f17 _pink"></div></div></tr><tr><div>
+                            
+                        <style type="text/css"></style>	
+                             	<div class="form-input"><input name="password" placeholder="Enter password*"  value="" size="20" type="password"  class="password  start__input _semi _f17 _pink  ">
+                            </div></div></tr>									
+                            </div></div><br><style type="text/css"></style><!-- <center> -->
+                                <button type="submit"class="button start__input start__input--submit _one _f17 _white _upper"data-hover="Login to account" id="WalletWithdrawalButton" ><div>
+                                    Login to account</div></button><!--   
+                                         <button  class="btn btn-lg btn-block btn-dark lift text-uppercase">
+                                            <span class="btn_page"></span></button> --><!-- </center> --></form>
+                                </div><a href="page.php=forgot.html" class="start__link _regular _f16">
+                                    Reset password </a></div><div class="start__form-footer _one _f13">
+  <!-- Privacy policy -->
+</div>
+</section>
+</main>
+</div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
+<script src="{{asset('')}}assets/js/app.min.js"></script>
+<script
+  src="https://code.jquery.com/jquery-3.7.1.min.js"
+  integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
+  crossorigin="anonymous"></script>
+<script>
+
+var container = document.getElementsByClassName("settings__password-new-repeat start__payment-password")[0];
+container.onkeyup = function(e) {
+    var target = e.srcElement;
+    var maxLength = parseInt(target.attributes["maxlength"].value, 10);
+    var myLength = target.value.length;
+    if (myLength >= maxLength) {
+        var next = target;
+        while (next = next.nextElementSibling) {
+            if (next == null)
+                break;
+            if (next.tagName.toLowerCase() == "input") {
+                next.focus();
+                break;
+            }
         }
-    </script>
+    }
+}
+
+</script>
+
 
 
 </body>
-
 </html>
